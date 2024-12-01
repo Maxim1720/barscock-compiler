58f2d112a0cdf3a7b0c876cbcfc57ec97cb95811
maxim1720 <musiev_maksim@mail.ru>
2024-12-01 16:38:05 +0500

    to check identifiers

--- backend/src/analyzer/symantic/analyzer/expression.py
+++ backend/src/analyzer/symantic/analyzer/expression.py
@@ -1,6 +1,15 @@
 #Задача анализа выражений - проверить описаны ли переменные, встре-
 #чающиеся в выражениях, и соответствуют ли типы операндов друг другу и типу
 #операции.
+from src.analyzer.symantic.files import get_identifiers_from_file
+# встречается в :
+# for .. to <exp>
+# white <exp>
+# if <exp>
+# as <exp>
+# write(<exp>
+
+
 from src.analyzer.syntax.reader import LexemReader
 
 
@@ -14,4 +23,6 @@ class IdentifiersExistsAnalyzer:
         self._reader = reader
 
     def analyze(self):
-        pass
+        identifier = self._reader.readed_lexem()
+        if not identifier in [x.name for x in get_identifiers_from_file()]:
+            raise SyntaxError(f"identifier '{identifier}' is undefined")
