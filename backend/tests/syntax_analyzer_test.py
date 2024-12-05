ad406e1a06438ab87252a63f0c27caa2e5957271
maxim1720 <musiev_maksim@mail.ru>
2024-12-05 13:37:05 +0500

    to update test file

--- backend/tests/syntax_analyzer_test.py
+++ backend/tests/syntax_analyzer_test.py
@@ -16,18 +16,18 @@ begin
     lol as 123;
     a as 5 plus 7;
     b as 7 min 0;
-    e as 7.5e+575;
-    d as c mult 3;
+    d as 7.5e+575;
+    d as 5.5 mult 3;
     e as true or ~false;
     f as false and false;
-    g as 010101b or 0101b;           
+    f as 010101b or 0101b;           
     s as 2343o;             
     b as 0101b;
     hex as 123ABCDh;        
     decFromBinary as 18;    
     decFromBinary2 as 18d;
     
-    if b LT a then 
+    if a LT b then 
         a as b
     else
         b as 5 plus b: 
@@ -40,7 +40,7 @@ begin
     
     write(a plus a plus a);
     
-    while b LT a do a as a plus 2
+    while e do a as g plus 2
 end.
 
     """
