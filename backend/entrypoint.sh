98ad2b7b8fcba6d19dbde307a82c58e6005b00ec
maxim1720 <musiev_maksim@mail.ru>
2024-12-05 15:39:53 +0500

    to add --reload

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Убедитесь, что переменные окружения корректно передаются
 
-exec uvicorn src.main:app --host=${HOST} --port=${BACKEND_PORT}
+exec uvicorn src.main:app --reload --host=${HOST} --port=${BACKEND_PORT}
