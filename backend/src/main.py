4bb29f33033d3e95ea19fd6d6712a2c547d5a392
maxim1720 <musiev_maksim@mail.ru>
2024-12-05 15:39:07 +0500

    to update routes

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,14 +1,40 @@
 from fastapi import FastAPI, Response
-from starlette.requests import Request
+from fastapi import Request
+from starlette.responses import JSONResponse
 
+from .analyzer.syntax.syntax_analyzer import SyntaxAnalyzer
+from .analyzer.syntax.tools import lex_table_from_file
 from .analyzer.lexical.lexical_analyzer import Analyzer
 from .conf import get_global_config
-from .files import read_out
+from .files import read_out, flush_out
 from .schema import CodeRequestSchema, AnalyzeResponseSchema
 
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+@app.exception_handler(TypeError)
+async def type_error_handler(request: Request, exc: TypeError):
+    return (JSONResponse(
+        status_code=400,
+        content={
+            "error": "type",
+            "message": str(exc)
+        }
+    ))
+
+
+@app.exception_handler(SyntaxError)
+async def value_error_handler(request: Request, exc: SyntaxError):
+    return JSONResponse(
+        status_code=400,
+        content={
+            "error": "syntax",
+            "message": str(exc)
+        }
+    )
+
 
 config = get_global_config()
 
@@ -21,10 +47,9 @@ app.add_middleware(
 )
 
 
-
-@app.post("/analyze", response_model=AnalyzeResponseSchema, status_code=200)
-async def root(code: CodeRequestSchema) -> AnalyzeResponseSchema:
-    # LexicalAnalyzer(code.data).analyze()
+@app.post("/lexical", response_model=AnalyzeResponseSchema, status_code=200)
+async def lexical(code: CodeRequestSchema) -> AnalyzeResponseSchema:
+    flush_out()
     state = Analyzer(code.data).analyze()
     tw = read_out("tw")
     tl = read_out("tl")
@@ -34,6 +59,9 @@ async def root(code: CodeRequestSchema) -> AnalyzeResponseSchema:
     return AnalyzeResponseSchema(tw=tw, tl=tl, tn=tn, ti=ti, state=state.name)
 
 
-
-
-
+@app.post("/syntax", status_code=200)
+async def syntax():
+    SyntaxAnalyzer(lex_table_from_file()).analyze()
+    return JSONResponse(status_code=200, content={
+        "message": "Syntax analyzer successfully finished"
+    })
