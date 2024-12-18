4c7d548ee0ec035a44e350fa1063c9c8ae4fad25
maxim1720 <musiev_maksim@mail.ru>
2024-12-19 01:21:27 +0500

    to split output and colorize errors

--- backend/src/analyzer/main.py
+++ backend/src/analyzer/main.py
@@ -1,22 +1,36 @@
 import subprocess
 
+from colorama import Fore, Style
+
 from src.analyzer.lex import *
 from src.analyzer.lex.analyze import analyze_lex
 from src.analyzer.syntax.analyze import analyze_syntax
-import os
+
 
 def main():
     with open(f'{os.getcwd()}/code.txt', 'r') as f:
         code = f.read()
-    analyze_lex(code)
-    print("Лексический анализ прошел успешно!")
-    analyze_syntax(code)
-    print("Синтаксический и семантический анализ прошли успешно")
+    try:
+        analyze_lex(code)
+        print("-"*100)
+        print("Лексический анализ прошел успешно!")
+        for i in result_table:
+            print(f"Результат для таблицы {i}:")
+            print(", ".join(result_table[i]))
+        print("-"*100)
+        analyze_syntax(code)
+        print("Синтаксический и семантический анализ прошли успешно")
+        print("-" * 100)
 
-    subprocess.run(['nasm', '-f', 'elf64',
-                    f'{os.getcwd()}/output.asm',
-                    '-o', f'{os.getcwd()}/output.o'])
+        subprocess.run(['nasm', '-f', 'elf64',
+                        f'{os.getcwd()}/output.asm',
+                        '-o', f'{os.getcwd()}/output.o'])
+        print("Объектный файл успешно скомпилирован!")
+    except Exception as e:
+        print(Fore.RED + str(e) + Style.RESET_ALL)
+    finally:
+        print("-" * 100)
 
-    print("Объектный файл успешно скомпилирован!")
+
 if __name__ == '__main__':
     main()
