593aec96ae68e7ab4061150b05fe6afd3f812fd6
maxim1720 <musiev_maksim@mail.ru>
2024-12-22 13:40:48 +0500

    to handle lexemes

--- backend/src/analyzer/lex/file.py
+++ backend/src/analyzer/lex/file.py
@@ -1,11 +1,12 @@
 import os.path
 
+from .writer import write
+
 
 def exists(content, path):
     if os.path.exists(path):
         with open(path, 'r') as f:
             for line in f.readlines():
-                # print(f'line:{line.strip()}')
                 if line.strip() == content:
                     return True
 
@@ -14,7 +15,7 @@ def exists(content, path):
 
 
 def is_tw(token):
-    with open(f'{os.getcwd()}/res/tw.txt', 'r') as f:
+    with open(os.path.join(os.getcwd(), 'res', "tw.txt"), 'r') as f:
         for line in f.readlines():
             if line.strip() == token:
                 return True
@@ -22,8 +23,33 @@ def is_tw(token):
 
 
 def is_tl(token):
-    with open(f'{os.getcwd()}/res/tl.txt', 'r') as f:
+    with open(os.path.join(os.getcwd(), 'res', "tl.txt"), 'r') as f:
         for line in f.readlines():
             if line.strip() == token:
                 return True
     return False
+
+def get_line_number(content, path):
+    with open(path, 'r') as f:
+        for index, line in enumerate(f.readlines()):
+            if line.strip() == content:
+                return index
+
+    return -1
+
+
+def write_lex(content, type):
+    table_dict = {
+        "tw": 1,
+        "tl": 2,
+        "ti": 3,
+        "tn": 4
+    }
+    src_dir = os.path.join('out','lex')
+    n = get_line_number(content, os.path.join(os.getcwd(), src_dir, f'{type}.txt'))
+    write(f"{table_dict[type]} {n}", os.path.join(os.getcwd(), "out", "lex", "lex.txt"))
+
+
+def read_lexemes():
+    with open(os.path.join(os.getcwd(), 'out', 'lex', 'lex.txt'), 'r') as f:
+        return map(lambda l: l.strip(), f.readlines())
