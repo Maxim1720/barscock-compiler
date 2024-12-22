1cd1d3145418f563a8a49fbc5f9d50523638ec1a
maxim1720 <musiev_maksim@mail.ru>
2024-12-22 13:48:32 +0500

    to make dir instead of file

--- backend/src/analyzer/lex/writer.py
+++ backend/src/analyzer/lex/writer.py
@@ -5,7 +5,7 @@ import re
 def write(content: str, path: str) -> None:
     if not os.path.exists(path):
         dir = os.path.split(path)
-        os.makedirs(dir[1], exist_ok=True)
+        os.makedirs(dir[0], exist_ok=True)
 
     with open(path, 'a') as f:
         f.write(f'{content}\n')
